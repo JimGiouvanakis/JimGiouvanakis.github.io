235f72c5a403d2d51e908d306ad101095fd18bb3
Dimitrios <jim41999@yahoo.gr>
2025-02-19 17:03:56 +0200

    Adjustments in the info

--- assets/js/title.js
+++ assets/js/title.js
@@ -1,17 +1,24 @@
 // Array containing different titles to cycle through
 var titles = [
   "@",
-  "@Y",
-  "@Yo",
-  "@Yog",
-  "@Yogh",
-  "@Yoghu",
-  "@Yoghur",
-  "@Yoghurt",
-  "@Yoghurt 1",
-  "@Yoghurt 13",
-  "@Yoghurt 133",
-  "@Yoghurt 1337"
+  "@D",
+  "@Di",
+  "@Dim",
+  "@Dimi",
+  "@Dimit",
+  "@Dimitr",
+  "@Dimitri",
+  "@Dimitris",
+  "@Dimitris G",
+  "@Dimitris Gi",
+  "@Dimitris Gio",
+  "@Dimitris Giou",
+  "@Dimitris Giouv",
+  "@Dimitris Giouva",
+  "@Dimitris Giouvan",
+  "@Dimitris Giouvana",
+  "@Dimitris Giouvanaki",
+  "@Dimitris Giouvanakis",
 ];
 
 // Function to change the title periodically
@@ -19,11 +26,11 @@ function changeTitle() {
   var index = 0; // Initialize index to start from the first title
 
   // Set interval to change the title every 1000 milliseconds (1 second)
-  setInterval(function() {
-      // Set the document title to the title at the current index
-      document.title = titles[index];
-      // Increment the index and use modulo operator to ensure it stays within the bounds of the array
-      index = (index + 1) % titles.length;
+  setInterval(function () {
+    // Set the document title to the title at the current index
+    document.title = titles[index];
+    // Increment the index and use modulo operator to ensure it stays within the bounds of the array
+    index = (index + 1) % titles.length;
   }, 1000); // Interval set to 1000 milliseconds (1 second)
 }
 
